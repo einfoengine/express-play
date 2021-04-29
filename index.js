9a8b958886d9903d3422c1f5f1ceaadbcbe811d6
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2021-04-30 05:15:56 +0600

    set route - sample with get method

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-
+app.get('/', (req, res)=>{
+    res.send("<h3>Welcome to express</h3>");
+});
 
 app.listen(port, () => {
     console.log(`The express server is running on ${port}`);
